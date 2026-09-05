4967e6258f554f35ae4904004f619f3b964bcdba
Nullskulls <nullskulls@proton.me>
2026-09-06 01:13:20 +0300

    Feat(handlers): Add MessageHandler class

--- source/handlers.py
+++ source/handlers.py
@@ -1,1 +1,20 @@
-def define_pruge_type():
+import time
+from slack_sdk.errors import SlackApiError
+
+class MessageHandler:
+
+    def purge_message(self, user_app, message_ts, channel, thread_ts=None):
+        try:
+            user_app.chat.delete(
+                channel=channel,
+                ts=message_ts,
+                thread_ts=thread_ts
+            )
+        except SlackApiError as e:
+            if e.response.status_code == 429:
+                retry_after = int(e.response.headers.get("Retry-After", 1))
+                time.sleep(retry_after)
+                self.purge_message(user_app, message_ts, channel, thread_ts)
+            else:
+                print(f"Error purging message: {e.response['error']}")
+
